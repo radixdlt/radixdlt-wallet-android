4fffc5c7fafb9562a117ba10ad8f148fe32282fe
Marc Rubio <marc@radixdlt.com>
2019-01-29 11:56:13 +0000

    Add dependencies and gradle setup to allow for instrumentation testing with Espresso. Add first instrumentation tests including reporting an issue.

---
+++ app/src/androidTest/java/com/radixdlt/android/activity/CreateNewWalletTest.kt
@@ -0,0 +1,74 @@
+package com.radixdlt.android.activity
+
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.rule.ActivityTestRule
+import com.radixdlt.android.R
+import com.radixdlt.android.helper.DelayHelper
+import com.radixdlt.android.helper.DelayHelper.idlingResource
+import com.radixdlt.android.ui.activity.NewWalletActivity
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo
+import com.schibsted.spain.barista.rule.cleardata.ClearDatabaseRule
+import com.schibsted.spain.barista.rule.cleardata.ClearFilesRule
+import com.schibsted.spain.barista.rule.cleardata.ClearPreferencesRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class CreateNewWalletTest {
+
+    /**
+     * [ActivityTestRule] is a JUnit [@Rule][Rule] to launch your activity under test.
+     *
+     * Rules are interceptors which are executed for each test method and are important building
+     * blocks of Junit tests.
+     */
+    @get:Rule var newWalletActivityTestRule: ActivityTestRule<NewWalletActivity> =
+        ActivityTestRule(NewWalletActivity::class.java)
+
+    // Clear all app's SharedPreferences
+    @get:Rule var clearPreferencesRule = ClearPreferencesRule()
+
+    // Delete all tables from all the app's SQLite Databases
+    @get:Rule var clearDatabaseRule = ClearDatabaseRule()
+
+    // Delete all files in getFilesDir() and getCacheDir()
+    @get:Rule var clearFilesRule = ClearFilesRule()
+
+    @Test
+    fun testCreatingNewWallet() {
+        createNewWallet()
+    }
+
+    companion object {
+        fun createNewWallet() {
+            // Click on fields to request focus
+            clickOn(R.id.createNewWalletButton)
+            writeTo(R.id.inputPasswordTIET, "123456")
+            writeTo(R.id.inputRepeatPasswordTIET, "123456")
+            clickOn(R.id.createWalletButton)
+
+            // Check progress dialog is showing with the correct label
+            assertDisplayed(R.string.enter_password_activity_creating_wallet_progress_dialog)
+
+            DelayHelper.waitTime(TimeUnit.SECONDS.toMillis(8))
+
+            // Check MainFragment has loaded by checking account balance title
+            assertDisplayed(R.string.wallet_fragment_xml_account_balance_title)
+
+            // Clean up
+            IdlingRegistry.getInstance().unregister(idlingResource)
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/radixdlt/android/fragment/MoreOptionsFragmentTest.kt
@@ -0,0 +1,74 @@
+package com.radixdlt.android.fragment
+
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import androidx.test.uiautomator.UiDevice
+import com.radixdlt.android.R
+import com.radixdlt.android.activity.CreateNewWalletTest
+import com.radixdlt.android.ui.activity.MainActivity
+import com.radixdlt.android.ui.activity.NewWalletActivity
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import com.schibsted.spain.barista.rule.cleardata.ClearDatabaseRule
+import com.schibsted.spain.barista.rule.cleardata.ClearFilesRule
+import com.schibsted.spain.barista.rule.cleardata.ClearPreferencesRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class MoreOptionsFragmentTest {
+
+    /**
+     * [ActivityTestRule] is a JUnit [@Rule][Rule] to launch your activity under test.
+     *
+     * Rules are interceptors which are executed for each test method and are important building
+     * blocks of Junit tests.
+     */
+    @get:Rule var newWalletActivityTestRule: ActivityTestRule<NewWalletActivity> =
+        ActivityTestRule(NewWalletActivity::class.java)
+
+    // Clear all app's SharedPreferences
+    @get:Rule
+    var clearPreferencesRule = ClearPreferencesRule()
+
+    // Delete all tables from all the app's SQLite Databases
+    @get:Rule var clearDatabaseRule = ClearDatabaseRule()
+
+    // Delete all files in getFilesDir() and getCacheDir()
+    @get:Rule
+    var clearFilesRule = ClearFilesRule()
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
+
+    @Test
+    fun testOpenReportIssueWebView() {
+        CreateNewWalletTest.createNewWallet()
+
+        clickOn(R.id.navigation_more_options)
+        assertDisplayed(R.string.more_options_fragment_xml_report_an_issue)
+
+        clickOn(R.id.reportAnIssueTextView)
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(6))
+
+        intended(toPackage("com.android.chrome"))
+
+        // Press the back button to exit app
+        val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        mDevice.pressBack()
+    }
+}
