7681893a63c738c238e1fed8c7b2e0de76c81970
Marc Rubio <marc@radixdlt.com>
2019-02-22 14:42:29 +0000

    Update dependencies and change gradle from groovy to kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.internal.AndroidExtensionsExtension
+
 plugins {
     id(BuildPlugins.androidApplication)
     id(BuildPlugins.kotlinAndroid)
@@ -15,7 +17,10 @@ android {
         versionCode = 6
         versionName = "0.15.00"
 
-        testInstrumentationRunnerArgument("runnerBuilder", "de.mannodermaus.junit5.AndroidJUnit5Builder")
+        testInstrumentationRunnerArgument(
+            "runnerBuilder",
+            "de.mannodermaus.junit5.AndroidJUnit5Builder"
+        )
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
 
@@ -26,7 +31,6 @@ android {
     }
 
     testOptions {
-//        unitTests.all(useJUnitPlatform())
         execution = "ANDROIDX_TEST_ORCHESTRATOR"
     }
 
@@ -76,12 +80,111 @@ android {
     }
 }
 
-//configurations(ktlint)
-//
-//task("ktlint") {
-//
-//}
+val ktlint by configurations.creating
+
+tasks {
+
+    check {
+        dependsOn(ktlint)
+    }
+
+    withType(Test::class) {
+        useJUnitPlatform()
+    }
+
+    create("ktlint", JavaExec::class) {
+        group = "verification"
+        description = "Check Kotlin code style."
+        classpath = ktlint
+        main = "com.github.shyiko.ktlint.Main"
+        args("src/**/*.kt")
+        // to generate report in checkstyle format prepend following args:
+        // "--reporter=plain", "--reporter=checkstyle,output=${buildDir}/ktlint.xml"
+        // see https://github.com/shyiko/ktlint#usage for more
+    }
+
+    create("ktlintFormat", JavaExec::class) {
+        group = "formatting"
+        description = "Fix Kotlin code style deviations."
+        classpath = ktlint
+        main = "com.github.shyiko.ktlint.Main"
+        args("-F", "src/**/*.kt")
+    }
+}
+
+androidExtensions {
+    configure(delegateClosureOf<AndroidExtensionsExtension> {
+        isExperimental = true
+    })
+}
 
 dependencies {
+    ktlint(Libraries.ktlint)
 
+    implementation(project(":radixdlt-java"))
+
+    implementation(Libraries.kotlinStdLib)
+    implementation(Libraries.ankoCommons)
+    implementation(Libraries.legacySupport)
+    implementation(Libraries.appCompat)
+    implementation(Libraries.material)
+    implementation(Libraries.cardView)
+    implementation(Libraries.fragment)
+    implementation(Libraries.lifeCycleExtensions)
+    implementation(Libraries.constraintLayout)
+    implementation(Libraries.ktxCore)
+    implementation(Libraries.browser)
+    implementation(Libraries.fireBaseCore)
+    implementation(Libraries.roomRuntime)
+    implementation(Libraries.roomRxJava)
+    kapt(Libraries.roomCompiler)
+    implementation(Libraries.navigationFragmentKtx)
+    implementation(Libraries.navigationUiKtx)
+    implementation(Libraries.playServicesVision)
+    implementation(Libraries.dagger)
+    implementation(Libraries.daggerAndroid)
+    kapt(Libraries.daggerAndroidCompiler)
+    kapt(Libraries.daggerCompiler)
+    implementation(Libraries.icu4j)
+    implementation(Libraries.circleImageView)
+    implementation(Libraries.qrGen)
+    implementation(Libraries.durationPicker)
+    implementation(Libraries.glide)
+    kapt(Libraries.glideCompiler)
+    implementation(Libraries.rxAndroid)
+    implementation(Libraries.rxKotlin) {
+        exclude("io.reactivex.rxjava2", "rxjava")
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+    }
+    implementation(Libraries.timber)
+    implementation(Libraries.threeTenABP)
+    implementation(Libraries.processPhoenix)
+    implementation(Libraries.vault)
+
+    testImplementation(TestLibraries.mockitoKotlin)
+    testImplementation(TestLibraries.mockk)
+    testImplementation(TestLibraries.junitJupiterApi)
+    testRuntimeOnly(TestLibraries.junitJupiterEngine)
+
+    testImplementation(TestLibraries.junitJupiterEngine)
+    testImplementation(TestLibraries.junit4)
+    testRuntimeOnly(TestLibraries.junitVintageEngine)
+
+    androidTestImplementation(TestLibraries.junitJupiterApi)
+    androidTestImplementation(TestLibraries.androidInstrumentationTest)
+
+    androidTestRuntimeOnly(TestLibraries.junitJupiterEngine)
+    androidTestRuntimeOnly(TestLibraries.junitPlatformRunner)
+    androidTestRuntimeOnly(TestLibraries.androidInstrumentationTest)
+
+    androidTestImplementation(TestLibraries.runner)
+    androidTestImplementation(TestLibraries.rules)
+    androidTestImplementation(TestLibraries.junit)
+    androidTestImplementation(TestLibraries.barista) {
+        exclude("com.android.support")
+        exclude("org.jetbrains.kotlin") // Only if you already use Kotlin in your project
+    }
+    androidTestImplementation(TestLibraries.espressoIntents)
+    androidTestImplementation(TestLibraries.coreTesting)
+    androidTestUtil(TestLibraries.orchestrator)
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -14,6 +14,9 @@ object BuildPlugins {
     const val kotlinAndroid = "kotlin-android"
     const val kotlinAndroidExtensions = "kotlin-android-extensions"
     const val kotlinKapt = "kotlin-kapt"
+//    const val kotlinAndroid = "android"
+//    const val kotlinAndroidExtensions = "android-extensions"
+//    const val kotlinKapt = "kapt"
     const val androidJunit5 = "de.mannodermaus.android-junit5"
 }
 
@@ -25,53 +28,75 @@ object AndroidSdk {
 
 object Libraries {
     private object Versions {
-        const val anko = "0.10.1"
-        const val appCompat = "1.0.2"
+        const val ktlint = "0.30.0"
+        const val ankoCommons = "0.10.8"
+        const val appCompat = "1.1.0-alpha02"
         const val jepack = "1.0.0"
         const val constraintLayout = "2.0.0-alpha3"
+        const val browser = "1.0.0"
+        const val firebaseCore = "16.0.7"
+        const val room = "1.1.1"
         const val ktx = "1.1.0-alpha04"
-        const val navVersion = "1.0.0-beta02"
+        const val navFragmentVersion = "1.0.0-rc01"
+        const val fragmentKtx = "1.1.0-alpha04"
+        const val lifeCycleExtentions = "2.1.0-alpha02"
         const val dagger = "2.21"
+        const val glide = "4.9.0"
+        const val material = "1.1.0-alpha03"
+        const val playServicesVision = "17.0.2"
+        const val icu = "63.1"
+        const val circleImageView = "3.0.0"
+        const val qrGen = "2.5.0"
+        const val rxAndroid = "2.1.1"
+        const val rxKotlin = "2.3.0"
+        const val timeDurationPicker = "1.1.3"
+        const val threeTenABP = "1.1.2"
+        const val timber = "4.7.1"
+        const val processPhoenix = "2.0.0"
     }
 
+    const val ktlint = "com.github.shyiko:ktlint:${Versions.ktlint}"
+
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
-    const val ankoCommons =  "org.jetbrains.anko:anko-commons:${Versions.anko}"
+    const val ankoCommons =  "org.jetbrains.anko:anko-commons:${Versions.ankoCommons}"
+    const val legacySupport = "androidx.legacy:legacy-support-v4:${Versions.jepack}"
     const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
     const val ktxCore = "androidx.core:core-ktx:${Versions.ktx}"
+    const val material = "com.google.android.material:material:${Versions.material}"
     const val cardView = "androidx.cardview:cardview:${Versions.jepack}"
-    const val fragment = "androidx.fragment:fragment-ktx:${Versions.jepack}"
-    const val lifeCycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+    const val fragment = "androidx.fragment:fragment-ktx:${Versions.fragmentKtx}"
+    const val lifeCycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifeCycleExtentions}"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
-    const val browser = "androidx.browser:browser:1.0.0"
-    const val fireBase = "com.google.firebase:firebase-core:16.0.7"
-    const val roomRuntime = "android.arch.persistence.room:runtime:1.1.1"
-    const val roomRxJava =  "android.arch.persistence.room:rxjava2:1.1.1"
-    const val roomCompiler = "android.arch.persistence.room:compiler:1.1.1"
+    const val browser = "androidx.browser:browser:${Versions.browser}"
+    const val fireBaseCore = "com.google.firebase:firebase-core:${Versions.firebaseCore}"
+    const val roomRuntime = "android.arch.persistence.room:runtime:${Versions.room}"
+    const val roomRxJava =  "android.arch.persistence.room:rxjava2:${Versions.room}"
+    const val roomCompiler = "android.arch.persistence.room:compiler:${Versions.room}"
 
-    const val navigationFragmentKtx = "android.arch.navigation:navigation-fragment-ktx:${Versions.navVersion}"
-    const val navigationUiKtx = "android.arch.navigation:navigation-fragment-ktx:${Versions.navVersion}"
+    const val navigationFragmentKtx = "android.arch.navigation:navigation-fragment-ktx:${Versions.navFragmentVersion}"
+    const val navigationUiKtx = "android.arch.navigation:navigation-fragment-ktx:${Versions.navFragmentVersion}"
 
-    const val playServicesVision = "com.google.android.gms:play-services-vision:17.0.2"
+    const val playServicesVision = "com.google.android.gms:play-services-vision:${Versions.playServicesVision}"
 
-    const val icu4j = "com.ibm.icu:icu4j:53.1"
+    const val icu4j = "com.ibm.icu:icu4j:${Versions.icu}"
 
-    const val circleImageView = "de.hdodenhof:circleimageview:2.2.0"
+    const val circleImageView = "de.hdodenhof:circleimageview:${Versions.circleImageView}"
 
-    const val qrGen = "com.github.kenglxn.QRGen:android:2.5.0"
+    const val qrGen = "com.github.kenglxn.QRGen:android:${Versions.qrGen}"
 
-    const val durationPicker = "mobi.upod:time-duration-picker:1.1.3"
+    const val durationPicker = "mobi.upod:time-duration-picker:${Versions.timeDurationPicker}"
 
-    const val glide = "com.github.bumptech.glide:glide:4.7.1"
-    const val glideCompiler = "com.github.bumptech.glide:compiler:4.7.1"
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
 
     // Rx
-    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
-    const val rxKotlin =  "io.reactivex.rxjava2:rxkotlin:2.2.0"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+    const val rxKotlin =  "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlin}"
 
     // JakeWharton
-    const val timber = "com.jakewharton.timber:timber:4.7.0"
-    const val threeTenABP = "com.jakewharton.threetenabp:threetenabp:1.0.5"
-    const val processPhoenix = "com.jakewharton:process-phoenix:2.0.0"
+    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+    const val threeTenABP = "com.jakewharton.threetenabp:threetenabp:${Versions.threeTenABP}"
+    const val processPhoenix = "com.jakewharton:process-phoenix:${Versions.processPhoenix}"
 
     const val vault = "com.bottlerocketstudios:vault:1.4.2"
 
@@ -85,27 +110,41 @@ object Libraries {
 object TestLibraries {
     private object Versions {
         const val junit4 = "4.13-beta-2"
+        const val mockitoKotlin = "1.5.0"
+        const val mockk = "1.9.1"
+        const val jupiter = "5.4.0"
+        const val androidIntrumentationTest = "0.2.2"
+        const val junitPlatformRunner = "1.4.0"
+        const val runner = "1.1.2-alpha01"
+        const val rules = "1.1.2-alpha01"
+        const val barista = "2.7.1"
+        const val espressoIntents = "3.1.2-alpha01"
+        const val junit = "1.1.1-alpha01"
+        const val coreTesting = "1.1.1"
+        const val orchestrator = "1.1.2-alpha01"
     }
 
-    const val mockitoKotlin = "com.nhaarman:mockito-kotlin:1.5.0"
-    const val mockk = "io.mockk:mockk:1.8.13.kotlin13"
+    const val mockitoKotlin = "com.nhaarman:mockito-kotlin:${Versions.mockitoKotlin}"
+    const val mockk = "io.mockk:mockk:${Versions.mockk}"
 
-    const val junitJupiterApi = "org.junit.jupiter:junit-jupiter-api:5.4.0"
-    const val junitJupiterEngine = "org.junit.jupiter:junit-jupiter-engine:5.4.0"
-    const val junitJupiterParams = "org.junit.jupiter:junit-jupiter-params:5.4.0"
+    const val junitJupiterApi = "org.junit.jupiter:junit-jupiter-api:${Versions.jupiter}"
+    const val junitJupiterEngine = "org.junit.jupiter:junit-jupiter-engine:${Versions.jupiter}"
+    const val junitJupiterParams = "org.junit.jupiter:junit-jupiter-params:${Versions.jupiter}"
 
     const val junit4 = "junit:junit:${Versions.junit4}"
-    const val junitVintageEngine = "org.junit.vintage:junit-vintage-engine:5.4.0"
+    const val junitVintageEngine = "org.junit.vintage:junit-vintage-engine:${Versions.jupiter}"
 
-    const val androidInstrumentationTest = "de.mannodermaus.junit5:android-instrumentation-test:0.2.2"
+    const val androidInstrumentationTest = "de.mannodermaus.junit5:android-instrumentation-test:${Versions.androidIntrumentationTest}"
 
-    const val junitPlatformRunner = "org.junit.platform:junit-platform-runner:1.4.0"
-    const val androidInstrumentationTestRunner = "de.mannodermaus.junit5:android-instrumentation-test-runner:0.2.2"
+    const val junitPlatformRunner = "org.junit.platform:junit-platform-runner:${Versions.junitPlatformRunner}"
+    const val androidInstrumentationTestRunner = "de.mannodermaus.junit5:android-instrumentation-test-runner:${Versions.androidIntrumentationTest}"
 
-    const val runner = "androidx.test:runner:1.1.1"
-    const val rules = "androidx.test:rules:1.1.1"
+    const val runner = "androidx.test:runner:${Versions.runner}"
+    const val rules = "androidx.test:rules:${Versions.rules}"
+    const val junit = "androidx.test.ext:junit:${Versions.junit}"
 
-    const val barista = "com.schibsted.spain:barista:2.7.0"
-    const val espressoIntents = "androidx.test.espresso:espresso-intents:3.1.1"
-    const val orchestrator = "androidx.test:orchestrator:1.1.1"
+    const val barista = "com.schibsted.spain:barista:${Versions.barista}"
+    const val espressoIntents = "androidx.test.espresso:espresso-intents:${Versions.espressoIntents}"
+    const val coreTesting = "android.arch.core:core-testing:${Versions.coreTesting}"
+    const val orchestrator = "androidx.test:orchestrator:${Versions.orchestrator}"
 }
