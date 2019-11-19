d016515ae1d11f465274f448111db2b3dcc6b91c
Marc Rubio <marc@radixdlt.com>
2019-11-19 15:55:37 +0000

    Add LearnFragment instrumentation test

---
+++ app/src/androidTest/java/com/radixdlt/android/apps/wallet/fragment/LearnFragmentTest.kt
@@ -0,0 +1,87 @@
+package com.radixdlt.android.apps.wallet.fragment
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.rule.ActivityTestRule
+import com.radixdlt.android.apps.wallet.R
+import com.radixdlt.android.apps.wallet.helper.clickOn
+import com.radixdlt.android.apps.wallet.helper.navigationIconMatcher
+import com.radixdlt.android.apps.wallet.ui.activity.StartActivity
+import com.schibsted.spain.barista.assertion.BaristaEnabledAssertions.assertEnabled
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import com.schibsted.spain.barista.rule.cleardata.ClearDatabaseRule
+import com.schibsted.spain.barista.rule.cleardata.ClearFilesRule
+import com.schibsted.spain.barista.rule.cleardata.ClearPreferencesRule
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class LearnFragmentTest {
+
+    /**
+     * [ActivityTestRule] is a JUnit [@Rule][Rule] to launch your activity under test.
+     *
+     * Rules are interceptors which are executed for each test method and are important building
+     * blocks of Junit tests.
+     */
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<StartActivity>()
+
+    // Clear all app's SharedPreferences
+    @get:Rule
+    var clearPreferencesRule = ClearPreferencesRule()
+
+    // Delete all tables from all the app's SQLite Databases
+    @get:Rule
+    var clearDatabaseRule = ClearDatabaseRule()
+
+    // Delete all files in getFilesDir() and getCacheDir()
+    @get:Rule
+    var clearFilesRule = ClearFilesRule()
+
+    @Before
+    fun setUp() {
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+
+    @Test
+    fun testNavigateToLearnBottomTabShowsWebView() {
+        createWallet()
+
+        // Click on x on the toolbar to dismiss
+        clickOn(navigationIconMatcher())
+
+        clickOn(R.id.menu_bottom_learn)
+        assertDisplayed(R.id.learnWebView)
+    }
+
+    private fun createWallet() {
+        onView(ViewMatchers.withId(R.id.greetingTermsAndConditionsCheckBox))
+            .perform(GreetingFragmentTest.clickIn(0, 0))
+        onView(ViewMatchers.withId(R.id.greetingPrivacyPolicyCheckBox))
+            .perform(GreetingFragmentTest.clickIn(0, 0))
+        assertEnabled(R.id.greetingGetStartedButton)
+        clickOn(R.id.greetingGetStartedButton)
+        assertDisplayed(R.string.create_wallet_fragment_welcome_title_xml)
+        clickOn(R.id.createWalletCreateNewWalletButton)
+    }
+}
